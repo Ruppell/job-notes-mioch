458ea8f71d442c31ae902fb4e938bdd0d0a6233a
Ephraim Kgwele <mekgwele@gmail.com>
2021-03-31 09:23:27 +0200

    update on post carousel

--- partials/blocks/home/posts-carousel.js
+++ partials/blocks/home/posts-carousel.js
@@ -2,17 +2,22 @@ document.addEventListener( 'DOMContentLoaded', function () {
 
     // Create the main slider.
     var postsSlider = new Splide( '#posts-slider', {
-    	type       : 'fade',
+    	type       : 'loop',
     	pagination : true,
     	arrows     : true,
-    	cover      : true,
-      rewind      : true,
-      // perPage: 4,
-      breakpoints : {
-    		'600': {
+      rewind     : true,
+      gap    : '1rem',
+      perPage: 4,
+    	breakpoints: {
+    		'640': {
+    			perPage: 2,
+    			gap    : '1rem',
+    		},
+    		'480': {
     			perPage: 1,
-    		}
-    	}
+    			gap    : '1rem',
+    		},
+      }
     } ).mount();
 
 
